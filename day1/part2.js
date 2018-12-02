829ea069d75c869e20cbce6fb8643bf0a3219d86
Mattias Rost <rrostt@gmail.com>
2018-12-02 08:44:33 +0100

    Remove commented code

--- day1/part2.js
+++ day1/part2.js
@@ -3,9 +3,7 @@ const fs = require('fs')
 fs.readFile('./input.txt', 'utf8', (err, content) => {
   const lines = content.split('\n')
   const numbers = lines.map(x => +x)
-//  const sum = numbers.reduce((sum, x) => sum + x)
 
-//  console.log(sum)
   const freqs = []
   let freq = 0
   let i = 0
